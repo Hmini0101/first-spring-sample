4e40beda3b0849c5fe6e15068af559cd43269415
Junyong Moon <hmp9511@naver.com>
2023-07-19 10:40:07 +0900

    init :first set

---
+++ src/main/java/com/example/demo/FirstBackExampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstBackExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstBackExampleApplication.class, args);
+	}
+
+}
